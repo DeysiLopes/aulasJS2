48ba91b8850fb1c4e843a836f15f871f49557d1a
DeysiLopes <deysilinconl@gmail.com>
2022-01-17 15:09:49 -0300

    finalizada aula coleções

--- colecoes.js
+++ colecoes.js
@@ -17,12 +17,42 @@
 
 // console.log(getAdmins(usuarios));
 
-const myArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
+// const myArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
 
-function valoresUnicos(arr){
-    const mySet = new Set(arr);
+// function valoresUnicos(arr){
+//     const mySet = new Set(arr);
 
-    return [...mySet];
+//     return [...mySet];
+// }
+
+// console.log(valoresUnicos(myArray));
+
+function validaArray (arr, num){
+    try {
+        
+            if(!arr && !num) throw new ReferenceError('Envie os parâmentros corretamente')
+        
+            if(typeof arr !== 'object') throw new TypeError('O array tem que ser do tipo object')
+        
+            if(typeof num !== 'number') throw new TypeError('O array tem que ser do tipo object')
+        
+        if(arr.length !== num) throw new RangeError('Tamanho invalido')  
+
+            return arr;
+    } catch (e) {
+        if(e instanceof TypeError){
+            console.log('Este é um typeError')
+            console.log(e.message)
+        } else if(e instanceof ReferenceError){
+            console.log('Este é um refenrenceError')
+            console.log(e.message)
+        } else if(e instanceof RangeError){
+            console.log('Este é um RangeError')
+            console.log(e.message)
+        } else{
+            console.log('Ocorreu um erro não esperado: ' + e)
+        }
+    }
 }
 
-console.log(valoresUnicos(myArray));
+console.log(validaArray([3,5,6,6,4],5))
